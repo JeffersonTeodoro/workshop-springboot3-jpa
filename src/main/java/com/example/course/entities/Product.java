3d4f029d8766e744fe542a385dfbc0ee1e1d42f1
jefferson F Teodoro <jeffersonfranca199@gmail.com>
2025-10-07 21:35:24 -0300

    Product

---
+++ src/main/java/com/example/course/entities/Product.java
@@ -0,0 +1,91 @@
+package com.example.course.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+
+@Entity
+@Table(name = "tb_product")
+public class Product implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imuUrl;
+
+    @Transient
+    private Set<Category> categories = new HashSet<>();
+
+
+    public Product() {
+    }
+
+    public Product(Integer id, String name, String description, Double price, String imuUrl) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imuUrl = imuUrl;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getImuUrl() {
+        return imuUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setImuUrl(String imuUrl) {
+        this.imuUrl = imuUrl;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
